9e2234a627c1541dd9ab9211550ac52b8147ea64
Viktor <victorharasymiv97@gmail.com>
2025-01-14 17:19:25 +0100

    goFixV3.0

---
+++ src/public/scripts/form.js
@@ -0,0 +1,31 @@
+const sendMessages = document.querySelector('.contact-us-form');
+
+sendMessages.addEventListener("submit", formMassage);
+
+function formMassage(event) {
+
+    event.preventDefault();
+
+    const reset = event.target;
+
+    const form = event.target.elements;
+
+    const email = form.email.value;
+    const name = form.name.value;
+    const phone = form.phone.value;
+    const textarea = form.textarea.value;
+    
+    alert(`Ваш е-мейл: ${email}
+           Вітаю Вас, ${name}
+           Ваш номер телефону: ${phone}
+           Ваше повідомлення: ${textarea}
+
+            Вас вітає команда курсу FullStack Developer
+                           CodeQuest
+                Ми бажаємо усім успіху та натхнення 
+                           До зустрічі !
+        `
+    );
+
+    reset.reset();
+}
